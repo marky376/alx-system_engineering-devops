72e41435a2f90a3d257d59114ea792424612f04e
marky376 <mmutuamark@gmail.com>
2024-01-31 04:23:27 +0300

    A commit on some files to work on

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,15 +1,1 @@
-#!/usr/bin/env ruby
 
-if ARGV.length != 1
-	puts "Usage: #{$PROGRAM_NAME} <text>"
-	exit 1
-
-end
-
-pattern = /school/
-
-text = ARGV[0]
-
-match = text.match(pattern)
-
-puts match ? match[0] : "$"
