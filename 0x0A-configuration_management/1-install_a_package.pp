abea192750ce322785db9aed73dc76fbcf3e393a
marky376 <mmutuamark@gmail.com>
2024-03-24 18:28:36 +0300

    A commit on learning the ruby language

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Install a package
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Install Werkzeug package
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,8 @@
-# Kills the killmenow process.
+# create a Puppet manifest that kills a process named "killmenow" using the exec resource and pkill
 
-exec { 'killmenow':
-  command => 'pkill killmenow',
-  path    => '/usr/local/bin/:/usr/bin:/bin/',
+exec { 'kill_killmenow_process':
+  command     => '/usr/bin/pkill killmenow',
+  path        => '/usr/bin:/bin',
+  refreshonly => true,
+  onlyif      => '/usr/bin/pgrep killmenow',
 }
