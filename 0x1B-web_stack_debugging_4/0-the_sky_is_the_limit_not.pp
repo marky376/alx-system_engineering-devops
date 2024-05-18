d641334f8021817fa738977b2efee44f4fe776ae
marky376 <mmutuamark@gmail.com>
2024-05-18 16:01:54 +0300

    Additions to change of permissions

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Fix Nginx open file limit configuration
+exec { 'nginx fix':
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx;  service nginx restart",
+  provider => 'shell'
+}
